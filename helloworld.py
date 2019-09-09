4b2b1fa608566abc03e00301438be7e72eb48fd0
skpalli <skpalli@umich.edu>
2019-09-09 17:44:41 -0400

    Pushing some python code

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+from random import randint
+
+print("Sanjay Kondapalli")
+
+num1 = randint(0, 100)
+num2 = randint(0,100)
+print(num1)
+print(num2)
+sum = num1+num2
+
+avg = sum/2
+
+print("Sum = " + str(sum))
+print("Average = " + str(avg))
+
